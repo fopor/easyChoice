8bb8f58b3f1d8db0553541c20a5e1fa1d94e4c3d
fopor <foporgit@gmail.com>
2018-02-10 13:18:46 -0200

    added confirmation popup between actions

--- js/votingSystem.js
+++ js/votingSystem.js
@@ -65,6 +65,7 @@ function updateChoiceList() {
     // clears the choice list before rebuilding
     choicesList.innerHTML = "";
 
+    // appende all the choices to the list
     if (choicesOptions.length >= 1) {
         document.getElementById("voting-list-desc").style.display = "block";
         document.getElementById("vote-start-button").style.display = "block";
@@ -87,10 +88,12 @@ function updateChoiceList() {
         }
     }  
 
+    // dont show the start voting button if we only have one choice
     if (choicesOptions.length == 1){
         document.getElementById("voting-list-desc").style.display = "block"; 
         document.getElementById("vote-start-button").style.display = "none";
     }
+
     if (choicesOptions.length == 0){
         document.getElementById("voting-list-desc").style.display = "none"; 
         document.getElementById("vote-start-button").style.display = "none";
@@ -99,46 +102,89 @@ function updateChoiceList() {
 
 function startVoting() {
     console.log("Starting voting...");
+
     if (choicesOptions.length <= 1) {
         console.log("Failed to start voting!");
         console.log("Invalid number of choices.");
     }
 
     else {
-        console.log("Starting voting...");
+        console.log("Prepare the pouop up confirmation...");
+        
+        document.getElementById("popup-message").innerHTML = "Start voting?";
+        document.getElementById("pop-cancel").innerHTML = "Cancel";
+        document.getElementById("pop-confirm").innerHTML = "Confirm";
 
-        // hides the option insert buttons
-        document.getElementById("optionCreator").style.display = "none";
-        document.getElementById("votArea").style.display = "block";
+        // add the logic to close the overlay
+        document.getElementById("pop-cancel").addEventListener("click", () => {
+            document.getElementById("overlay").style.display = "none";
 
+            // removes the listner
+            removeAllListners(document.getElementById('overlay'));
+        });
 
-        // creates the voting options
-        for (let choice of choicesOptions) {
-            let el = document.createElement("LI");
-            let tex = document.createTextNode("  " + choice.getChoiceName());
-            let btn = document.createElement("BUTTON");
-            btn.className = "btn-success";
-            el.onclick = function () {
-                voteOn(choice.getChoiceName());
-                totalOfVotes += 1;
-                document.getElementById('votCount').innerHTML = String(totalOfVotes);
+        // confirm the option
+        document.getElementById("pop-confirm").addEventListener("click", () => {
+            document.getElementById("overlay").style.display = "none";
+            
+            // removes the listner
+            removeAllListners(document.getElementById('overlay'));
+            
+            console.log("Starting voting...");
 
-                // visual click feedback
-                el.style.transition = "300ms";
-                el.style.backgroundColor = "grey";
-                setTimeout(function () {
-                    el.style.backgroundColor = "white";
-                  }, 300);
-            };
-            let t = document.createTextNode("Vote");
-            btn.appendChild(t);
-            // TODO: change this buttons style
-            // el.appendChild(btn);
-            el.className = "list-group-item";
-            el.appendChild(tex);
+            // hides the option insert buttons
+            document.getElementById("optionCreator").style.display = "none";
+            document.getElementById("votArea").style.display = "block";
 
-            document.getElementById('answersBox').appendChild(el);
-        }
+            // creates the voting options
+            document.getElementById('answersBox').innerHTML = "";
+            for (let choice of choicesOptions) {
+                let el = document.createElement("LI");
+                let tex = document.createTextNode("  " + choice.getChoiceName());
+                let btn = document.createElement("BUTTON");
+                btn.className = "btn-success";
+                el.onclick = function () {
+                    voteOn(choice.getChoiceName());
+                    totalOfVotes += 1;
+                    document.getElementById('votCount').innerHTML = String(totalOfVotes);
+
+                    // visual click feedback
+                    el.style.transition = "300ms";
+                    el.style.backgroundColor = "grey";
+                    setTimeout(function () {
+                        el.style.backgroundColor = "white";
+                    }, 300);
+                };
+                let t = document.createTextNode("Vote");
+                btn.appendChild(t);
+                // TODO: change this buttons style
+                // el.appendChild(btn);
+                el.className = "list-group-item";
+                el.appendChild(tex);
+
+                document.getElementById('answersBox').appendChild(el);
+            }
+        });
+
+        // show the overlay
+        document.getElementById("overlay").style.display = "block";
+    }
+}
+
+function removeAllListners(old_element) {
+    let new_element = old_element.cloneNode(true);
+    old_element.parentNode.replaceChild(new_element, old_element);
+}
+
+// goes back to the insert page from the voting page
+function backToInputList() {
+    document.getElementById("optionCreator").style.display = "block";
+    document.getElementById("votArea").style.display = "none";
+
+    // resets the number of votes
+    numberOfVotes = 0;
+    for(choice in choicesOptions) {
+        choice.numberOfVotes = 0;
     }
 }
 
@@ -161,10 +207,7 @@ function deleteMe(info) {
         if (choicesOptions[index].name === info) flag = false;
     }
 
-
-
     choicesOptions.splice(index - 1, 1);
-
 
     updateChoiceList();
 }
@@ -178,28 +221,52 @@ function getResult() {
 
     console.log("Getting result!");
 
-    for (let i = 0; i < choicesOptions.length; i++) {
-        currentName = choicesOptions[i].getChoiceName();
-        currentPoints = choicesOptions[i].getNumberOfVotes();
-        console.log("Reading: " + currentName + "-" + currentPoints + ".");
+    console.log("Prepare the pouop up confirmation...");
+        
+        document.getElementById("popup-message").innerHTML = "Show winner?";
+        document.getElementById("pop-cancel").innerHTML = "Back";
+        document.getElementById("pop-confirm").innerHTML = "Show";
 
-        //TODO resolve draw
-        if (currentPoints == winnerPoints) {
-            console.log("Draw!");
-            winnerName = winnerName + " and " + currentName;
-        }
+        // add the logic to close the overlay
+        document.getElementById("pop-cancel").addEventListener("click", () => {
+            document.getElementById("overlay").style.display = "none";
 
-        // new winner
-        else if (currentPoints > winnerPoints) {
-            winnerPoints = currentPoints;
-            winnerName = currentName;   
-        }
-    }
+            // removes the listner
+            removeAllListners(document.getElementById('overlay'));
+        });
 
-    console.log("Winner>" + winnerName);
-    document.getElementById("optionCreator").style.display = "none";
-    document.getElementById("votArea").style.display = "none";
-    document.getElementById("winner-box").style.display = "block";
-    
-    document.getElementById("winner-name").innerHTML = winnerName;
+        // confirm the option
+        document.getElementById("pop-confirm").addEventListener("click", () => {
+            document.getElementById("overlay").style.display = "none";
+
+            for (let i = 0; i < choicesOptions.length; i++) {
+                currentName = choicesOptions[i].getChoiceName();
+                currentPoints = choicesOptions[i].getNumberOfVotes();
+                console.log("Reading: " + currentName + "-" + currentPoints + ".");
+        
+                //TODO resolve draw
+                if (currentPoints == winnerPoints) {
+                    console.log("Draw!");
+                    winnerName = winnerName + " and " + currentName;
+                }
+        
+                // new winner
+                else if (currentPoints > winnerPoints) {
+                    winnerPoints = currentPoints;
+                    winnerName = currentName;   
+                }
+            }
+        
+            console.log("Winner>" + winnerName);
+            document.getElementById("optionCreator").style.display = "none";
+            document.getElementById("votArea").style.display = "none";
+            document.getElementById("winner-box").style.display = "block";
+            document.getElementById("winner-name").innerHTML = winnerName;
+            
+            // removes the listner
+            removeAllListners(document.getElementById('overlay'));
+        });
+
+        // show the overlay
+        document.getElementById("overlay").style.display = "block";
 }
